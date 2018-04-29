4a19bcb74678f33fbf9ad49ebbc14db053d9985b
Guillaume Marcais <gmarcais@cs.cmu.edu>
2018-04-29 12:43:18 +0200

    Use allocator.

--- include/compact_index.hpp
+++ include/compact_index.hpp
@@ -17,9 +17,10 @@ namespace index_imp {
 
 template<typename IDX, typename W, typename Allocator, unsigned int UB, bool TS>
 class index {
+  Allocator          m_allocator;
   const size_t       m_size;      // Size in number of element
   const unsigned int m_bits;      // Number of bits in an element
-  W*                 mem;
+  W*                 m_mem;
 
   static inline int clz(unsigned int x) { return __builtin_clz(x); }
   static inline int clz(unsigned long x) { return __builtin_clzl(x); }
@@ -42,10 +43,11 @@ public:
   typedef compact::const_iterator<IDX, W, UB>  const_iterator;
   typedef compact::iterator<IDX, W, true, UB>  mt_iterator; // Multi thread safe version
 
-  index(size_t s, unsigned int b)
-    : m_size(s)
+  index(size_t s, unsigned int b, Allocator allocator = Allocator())
+    : m_allocator(allocator)
+    , m_size(s)
     , m_bits(b)
-    , mem(new W[elements_to_words(m_size, m_bits)]) {
+    , m_mem(m_allocator.allocate(elements_to_words(m_size, m_bits))) {
     static_assert(UB <= bitsof<W>::val, "used_bits must be less or equal to the number of bits in the word_type");
     if(b > UB)
       throw std::out_of_range("Number of bits larger than usable bits");
@@ -54,18 +56,18 @@ public:
     : index(s, required_bits(s))
   { }
   ~index() {
-    delete [] mem;
+    m_allocator.deallocate(m_mem, elements_to_words(m_size, m_bits));
   }
 
-  const_iterator begin() const { return const_iterator(mem, m_bits, 0); }
-  iterator begin() { return iterator(mem, m_bits, 0); }
+  const_iterator begin() const { return const_iterator(m_mem, m_bits, 0); }
+  iterator begin() { return iterator(m_mem, m_bits, 0); }
   const_iterator end() const { return begin() + m_size; }
   iterator end() { return begin() + m_size; }
   const_iterator cbegin() const { return begin(); }
   const_iterator cend() const { return end(); }
 
   // Multi thread safe iterator
-  mt_iterator mt_begin() { return mt_iterator(mem, m_bits, 0); }
+  mt_iterator mt_begin() { return mt_iterator(m_mem, m_bits, 0); }
   mt_iterator mt_end() { return begin() + m_size; }
 
   IDX operator[](size_t i) const { return cbegin()[i]; }
@@ -73,8 +75,8 @@ public:
   size_t size() const { return m_size; }
   unsigned int bits() const { return m_bits; }
 
-  W* get() { return mem; }
-  const W* get() const { return mem; }
+  W* get() { return m_mem; }
+  const W* get() const { return m_mem; }
   size_t bytes() const { return sizeof(W) * elements_to_words(m_size, m_bits); }
 };
 } // namespace index_imp
